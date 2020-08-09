7f503909afe0fdd6561799fc6375e28c180248db
Bartosz <bkolodziejczak90@gmail.com>
2020-08-09 23:38:24 +0200

    dodanie testów dla bikeStationDetails i bikesStationsList

--- src/app/features/bike-station/components/bike-station-details/bike-station-details.component.spec.ts
+++ src/app/features/bike-station/components/bike-station-details/bike-station-details.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BikeStationDetailsComponent } from './bike-station-details.component';
 import { StationCardComponent } from '../station-card/station-card.component';
 import { MapsAPILoader } from '@agm/core';
+import { DebugElement } from '@angular/core';
 
 describe('BikeStationDetailsComponent', () => {
   let component: BikeStationDetailsComponent;
@@ -24,10 +25,22 @@ describe('BikeStationDetailsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BikeStationDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('if error exists should show error message in html', () => {
+    component.error = 'Error';
+    fixture.detectChanges();
+
+    const errorMessageDe: DebugElement = fixture.debugElement;
+    const errorMessageEL: HTMLElement = errorMessageDe.nativeElement;
+    const errorMessage = errorMessageEL.querySelector('.alert-danger');
+    const errorTextExists: boolean = errorMessage.textContent.includes(component.error);
+    expect(errorTextExists).toBeTruthy();
+  });
+
+
 });
--- src/app/features/bike-station/components/bikes-stations-list/bikes-stations-list.component.spec.ts
+++ src/app/features/bike-station/components/bikes-stations-list/bikes-stations-list.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BikesStationsListComponent } from './bikes-stations-list.component';
 import { StationCardComponent } from '../station-card/station-card.component';
+import { DebugElement } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
 
 describe('BikesStationsListComponent', () => {
   let component: BikesStationsListComponent;
@@ -12,6 +14,9 @@ describe('BikesStationsListComponent', () => {
       declarations: [
         BikesStationsListComponent,
         StationCardComponent,
+      ],
+      providers: [
+        MapsAPILoader
       ]
     })
       .compileComponents();
@@ -20,10 +25,48 @@ describe('BikesStationsListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BikesStationsListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('if error exists should show error message in html', () => {
+    component.error = 'Error';
+    fixture.detectChanges();
+
+    const errorMessageDe: DebugElement = fixture.debugElement;
+    const errorMessageEL: HTMLElement = errorMessageDe.nativeElement;
+    const errorMessage = errorMessageEL.querySelector('.alert-danger');
+    const errorTextExists: boolean = errorMessage.textContent.includes(component.error);
+    expect(errorTextExists).toBeTruthy();
+  });
+
+  it('clicked station should navgiate to map ', () => {
+    component.bikeStations = [
+      {
+        id: '1',
+        geometry: {
+          coordinates: [10, 20],
+          type: ''
+        },
+        type: '',
+        properties: {
+          free_racks: 10,
+          bikes: 5,
+          label: 'Test label',
+          bike_racks: 7,
+          updated: new Date(),
+        },
+      }
+    ];
+    spyOn(component.navigateToMap, 'emit');
+    fixture.detectChanges();
+    const stationCardDe: DebugElement = fixture.debugElement;
+    const stationCardEl: HTMLElement = stationCardDe.nativeElement.querySelector('.card');
+    stationCardEl.click();
+    fixture.detectChanges();
+    expect(component.navigateToMap.emit).toHaveBeenCalled();
+  });
+
 });
